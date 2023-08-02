4c28df1febfe20a4cc261c1f1155303198386312
babymason <33733326+babymason@users.noreply.github.com>
2023-08-01 19:36:45 -0700

    added responsive support

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import HeaderFooterLayout from "./components/HeaderFooterLayout";
+import Landing from "./pages/Landing";
+
+const App = () => {
+
+    return (
+
+        <BrowserRouter>
+            <Routes>
+                <Route element={<HeaderFooterLayout />}>
+                    <Route path="/" exact element={<Landing />} />
+                </Route>
+            </Routes>
+        </BrowserRouter>
+    );
+};
 
 export default App;
---
+++ src/pages/Landing.js
@@ -0,0 +1,11 @@
+
+const Landing = () => {
+
+    return (
+        <div className="grid grid-col-1">
+            <img className="w-full h-full"/>
+        </div>
+    );
+};
+
+export default Landing;
